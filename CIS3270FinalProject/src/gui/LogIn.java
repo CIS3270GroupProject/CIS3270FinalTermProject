223cca84fa5326b41d83a9049fc5de35007cf9b5
Wavmaker <jatlas73@gmail.com>
2018-12-04 18:52:38 -0500

    Welp

--- CIS3270FinalProject/src/gui/LogIn.java
+++ CIS3270FinalProject/src/gui/LogIn.java
@@ -1,5 +1,116 @@
 package gui;
 
+import javax.swing.*;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Label;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.ShellEvent;
+
 public class LogIn {
+	private JFrame yerr;
+	private static Text UserName;
+	private static Text Pswd;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Display display = Display.getDefault();
+		Shell shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		
+		Label LUser = new Label(shell, SWT.NONE);
+		LUser.setBounds(83, 35, 55, 15);
+		LUser.setText("Username:");
+		
+		Label LPswd = new Label(shell, SWT.NONE);
+		LPswd.setBounds(83, 77, 55, 15);
+		LPswd.setText("Password:");
+		
+		UserName = new Text(shell, SWT.BORDER);
+		UserName.setBounds(222, 29, 76, 21);
+		
+		Pswd = new Text(shell, SWT.BORDER);
+		Pswd.setBounds(222, 74, 76, 21);
+		
+		Button btnSignIn = new Button(shell, SWT.NONE);
+
+		btnSignIn.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				abstract class button1 implements ActionListener
+				{
+					LogIn type;
+					char[] cCheck, cPassword={'1','0','1','7','!','\0'};
+					JFrame f;
+					String sCheck,sCheck1="yeerrr";
+
+					public button1(LogIn type)
+					{
+						this.type = type;
+					
+				
+										}
+					public boolean check()
+					{
+						if (cCheck.length >= 6 || cCheck.length < 4)
+							return false;
+						for(int i=0;i<=4;i++)
+						{
+							if(cCheck[i]!=cPassword[i])
+								return false;
+						}
+						return true;
+					}
+				
+
+				
+			}
+			}
+		}
+		);
+		btnSignIn.setBounds(150, 146, 75, 25);
+		btnSignIn.setText("Sign In");
+
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+
+	public static void open() {
+		EventQueue.invokeLater(new Runnable(){
+			public void run(){
+				try{
+					LogIn window = new LogIn();
+					window.yerr.setVisible(true);
+				} catch (Exception e){
+					System.out.println("You got an error, fam");
+				}
+			}
+		});
+	
+		
+	}
 
 }
--- CIS3270FinalProject/src/gui/MainMenu.java
+++ CIS3270FinalProject/src/gui/MainMenu.java
@@ -1,5 +1,75 @@
 package gui;
 
-public class MainMenu {
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;;
 
+public class MainMenu {
+	private static Table table;
+	private static Table table_1;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Display display = Display.getDefault();
+		Shell shlAeroplane = new Shell();
+		shlAeroplane.setSize(450, 300);
+		shlAeroplane.setText("Plane Aeros");
+		
+		Composite composite = new Composite(shlAeroplane, SWT.NONE);
+		composite.setBounds(10, 10, 140, 194);
+		
+		Button btnTicket = new Button(composite, SWT.PUSH);
+		btnTicket.setEnabled(false);
+		btnTicket.setBounds(34, 148, 75, 25);
+		btnTicket.setText("Buy Ticket");
+		
+		Button btnRegister = new Button(composite, SWT.PUSH);
+		btnRegister.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+			}
+		});
+		btnRegister.setText("Registration");
+		btnRegister.setBounds(34, 88, 75, 25);
+		
+		Button btnSignIn = new Button(composite, SWT.PUSH);
+		btnSignIn.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				LogIn nw = new LogIn();
+				nw.open();
+			}
+		});
+		btnSignIn.setBounds(34, 26, 75, 25);
+		btnSignIn.setText("Sign In");
+		
+		TableViewer tableViewer = new TableViewer(shlAeroplane, SWT.BORDER | SWT.FULL_SELECTION);
+		table = tableViewer.getTable();
+		table.setBounds(186, 10, 220, 194);
+		
+		table_1 = new Table(shlAeroplane, SWT.BORDER | SWT.FULL_SELECTION);
+		table_1.setBounds(186, 10, 220, 194);
+		table_1.setHeaderVisible(true);
+		table_1.setLinesVisible(true);
+
+		shlAeroplane.open();
+		shlAeroplane.layout();
+		while (!shlAeroplane.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
 }
